1406ecad651ae341d177433d8f0f2274584b287d
gyupp <kkb75281@gmail.com>
2025-07-18 11:59:01 +0900

    design: client-secret 작업중

--- src/views/service/client_secret.vue
+++ src/views/service/client_secret.vue
@@ -27,6 +27,14 @@ section
 section
     .table-menu-wrap
         .table-functions
+            button.inline.only-icon.gray.sm(@click.stop="(e)=>{showDropDown(e)}")
+                svg.svgIcon
+                    use(xlink:href="@/assets/img/material-icon.svg#icon-checklist-rtl")
+                .moreVert(@click.stop style="--moreVert-left:0;display:none;font-weight:normal;")
+                    .inner(style="padding: 0.5rem;")
+                        Checkbox(v-model="filterOptions.name" style="display:flex;") Name
+                        Checkbox(v-model="filterOptions.client_secret" style="display:flex") Client Secret
+                        Checkbox(v-model="filterOptions.locked" style="display:flex") Locked
             button.inline.only-icon.gray.sm(@click="getPage(true)" :class="{ disabled: fetching || !user?.email_verified || currentService.service.active <= 0 }")
                 svg.svgIcon
                     use(xlink:href="@/assets/img/material-icon.svg#icon-refresh")
@@ -61,7 +69,7 @@ Table(:class="{'nonClickable' : !user?.email_verified || currentService.service.
             tr(v-for="i in 10")
                 td(:colspan="colspan")
         template(v-else)
-            tr.hoverRow(v-for="(cs, i) in listDisplay" @click="showDetail=true; selectedClient=cs")
+            tr.hoverRow(v-for="(cs, i) in listDisplay" @click="openDetailModal(cs)")
                 td
                     Checkbox(@click.stop
                         :modelValue="!!checked?.[cs?.name]"
@@ -138,6 +146,38 @@ Table(:class="{'nonClickable' : !user?.email_verified || currentService.service.
                             svg.svgIcon.reactive.clickable.hide(@click="deleteClientKey = !!key.name;deleteIndex = index;")
                                 use(xlink:href="@/assets/img/material-icon.svg#icon-delete-fill")
 
+// modal :: client secret key detail
+Modal.modal-scroll.modal-detailClient(:open="showDetail" @close="closeDetailModal")
+    form.modal-container(@submit.prevent='saveKey')
+        .modal-header
+            h4.title Client Secret Key
+            button.btn-close(type="button" @click="closeDetailModal")
+                svg.svgIcon
+                    use(xlink:href="@/assets/img/material-icon.svg#icon-close")
+        .modal-body
+            .content(v-if="showDetail")
+                label.row
+                    .key Name
+                    .value
+                        input#keyName.block(type="text" v-model="selectedClient.name" placeholder="myapi" required maxlength="16" @input="e=>e.target.setCustomValidity('')" :disabled='updating')
+                label.row
+                    .key Client Secret
+                    .value
+                        input.block(type="text" v-model="selectedClient.client_secret" placeholder="string1234..." required :disabled='updating')
+                label.row
+                    .key Locked &nbsp;
+                        Tooltip(tip-background-color="var(--main-color)" text-color="white")
+                            template(v-slot:tip)
+                                | When LOCKED only signed users can have access to the client secret key.
+                    .value
+                        Checkbox(v-model="selectedClient.locked" :disabled='updating' style="font-weight:unset;")
+        .modal-footer
+            template(v-if="updating")
+                .loader(style="--loader-color:blue; --loader-size:12px; margin: 12px;")
+            template(v-else)
+                button.btn-save(type="submit") SAVE
+
+// modal :: delete client secret key
 Modal(:open="deleteClientKey" @close="deleteClientKey=false")
     .modal-title Delete Client Secret
 
@@ -164,6 +204,7 @@ import Tooltip from '@/components/tooltip.vue';
 import { type Ref, ref, nextTick } from 'vue';
 import { user } from '@/code/user';
 import { currentService } from '@/views/service/main';
+import { showDropDown } from "@/assets/js/event.js";
 
 // table columns
 let filterOptions = ref({
@@ -173,6 +214,9 @@ let filterOptions = ref({
 });
 
 let selectedClient = ref(null);
+let backupClient = ref(null);
+let showDetail = ref(false);
+let uploading = ref(false);
 
 let fetching = ref(false);
 let updating = ref(false);
@@ -212,13 +256,32 @@ if (currentService.service.client_secret) {
     }
 }
 
+let openDetailModal = (cs) => {
+    console.log('openDetailModal', cs);
+    backupClient.value = JSON.stringify(cs);
+    selectedClient.value = cs;
+    showDetail.value = true;
+};
+
+let closeDetailModal = () => {
+    if (backupClient.value !== JSON.stringify(selectedClient.value)) {
+        if (confirm('You have unsaved changes. Do you want to discard them?')) {
+            showDetail.value = false;
+            selectedClient.value = null;
+        }
+    } else {
+        showDetail.value = false;
+        selectedClient.value = null;
+    }
+};
+
 let checked: Ref<{ [key: string]: any }> = ref({});
 function checkall() {
     if (Object.keys(checked.value).length) {
         checked.value = {};
     } else {
-        listDisplay.value.forEach((user) => {
-            checked.value[user.record_id] = user;
+        listDisplay.value.forEach((client) => {
+            checked.value[client.name] = client;
         });
     }
 }
@@ -289,51 +352,122 @@ let cancelKey = (key, index) => {
     }
 }
 
-let saveKey = async (key) => {
+let saveKey = async () => {
     let authKeys = [];
     let secKeys = {};
-    for (let i = 0; i < client_key.value.length; i++) {
-        let ck = client_key.value[i];
 
-        if (secKeys[ck.name] && secKeys[ck.name] !== ck.key) {
-            let el = document.getElementById('keyName');
-            el.focus();
-            el.setCustomValidity('The name is already in use.');
-            el.reportValidity();
-            return;
-        }
-
-        if (ck.secure && authKeys.indexOf(ck.name) === -1) {
-            authKeys.push(ck.name);
-        }
-
-        Object.assign(secKeys, {
-            [ck.name]: ck.key
-        });
+    // Validate selectedClient
+    if (!selectedClient.value.name || !selectedClient.value.client_secret) {
+        let el = document.getElementById('keyName');
+        el.focus();
+        el.setCustomValidity('Please fill in the name and client secret.');
+        el.reportValidity();
+        return;
     }
 
-    // loading...
-    updating.value = true;
-    await currentService.setServiceOption({
-        client_secret: secKeys,
-        auth_client_secret: authKeys,
-    }).catch(err => {
-        window.alert(err.message || err);
-    });
-
-    // loading end
-    updating.value = false;
-
-    if (addMode.value) {
-        key.edit = false;
-        addMode.value = false;
-    } else {
-        key.edit = false;
-        editMode.value = false;
+    // Check for duplicate names
+    if (listDisplay.value[selectedClient.value.name] && listDisplay.value[selectedClient.value.name] !== selectedClient.value.client_secret) {
+        let el = document.getElementById('keyName');
+        el.focus();
+        el.setCustomValidity('The name is already in use.');
+        el.reportValidity();
+        return;
     }
+
+    // for (let i=0; i<listDisplay.value.length; i++) {
+    //     let ck = listDisplay.value[i];
+
+    //     if (ck.locked && authKeys.indexOf(ck.name) === -1) {
+    //         authKeys.push(ck.name);
+    //     }
+
+    //     Object.assign(secKeys, {
+    //         [ck.name]: ck.client_secret
+    //     });
+    // }
+
+    // for (let i = 0; i < client_key.value.length; i++) {
+    //     let ck = client_key.value[i];
+
+    //     if (secKeys[ck.name] && secKeys[ck.name] !== ck.key) {
+    //         let el = document.getElementById('keyName');
+    //         el.focus();
+    //         el.setCustomValidity('The name is already in use.');
+    //         el.reportValidity();
+    //         return;
+    //     }
+
+    //     if (ck.secure && authKeys.indexOf(ck.name) === -1) {
+    //         authKeys.push(ck.name);
+    //     }
+
+    //     Object.assign(secKeys, {
+    //         [ck.name]: ck.key
+    //     });
+    // }
+
+    // // loading...
+    // updating.value = true;
+    // await currentService.setServiceOption({
+    //     client_secret: secKeys,
+    //     auth_client_secret: authKeys,
+    // }).catch(err => {
+    //     window.alert(err.message || err);
+    // });
+
+    // // loading end
+    // updating.value = false;
+
+    // if (addMode.value) {
+    //     key.edit = false;
+    //     addMode.value = false;
+    // } else {
+    //     key.edit = false;
+    //     editMode.value = false;
+    // }
 }
 </script>
 <style scoped lang="less">
+.content {
+    flex-grow: 1;
+    overflow-y: auto;
+    padding: 20px;
+    font-size: 0.8rem;
+
+    .value {
+        min-width: fit-content;
+        width: fit-content;
+        margin: 0;
+        flex: 1;
+    }
+
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        margin-bottom: 1.5rem;
+        margin-top: 0;
+        gap: 0.5rem;
+
+        &:last-of-type {
+            margin-bottom: 0;
+        }
+
+        &.line {
+            border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+            padding-bottom: 2rem;
+            margin-bottom: 2rem;
+        }
+    }
+
+    .key {
+        display: flex;
+        align-items: center;
+        font-weight: 500;
+        width: 170px;
+    }
+}
+
 .table-form {
     position: relative;
 
