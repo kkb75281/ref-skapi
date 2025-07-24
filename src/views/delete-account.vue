6ad2d3c6ea1fd174bb04a6fe7c33dfa54c4e8547
gyupp <kkb75281@gmail.com>
2025-07-24 16:48:10 +0900

    design: delete account page

--- src/views/delete-account.vue
+++ src/views/delete-account.vue
@@ -1,50 +1,45 @@
 <template lang="pug">
-br
-br
-br
-
-#confirmation 
-    router-link(to="/")
-        img(src="@/assets/img/logo/symbol-logo.png" style="width: 40px;")
-
-    .bottomLineTitle Delete Account
-
-    p Once your account is deleted, you will not be able to recover it.
-
-    p Please note:
-    ul(style='padding-left: 1em;')
-        li All your data will be deleted permanently.
-
-        li All your subscription plans will be cancelled.
-
-        li All your services will be removed.
-
-        li All your subdomains will be removed immediately.
-
-        li The remaining subscription cannot be refunded.
+main#confirmation 
+    button.inline.icon-text.dark.btn-prev(type="button" @click="router.push('/account-setting')")
+        svg
+            use(xlink:href="@/assets/img/material-icon.svg#icon-arrow-back-ios")
+        span Back
 
     br
-    
-    p Are you sure you want to delete your account?
 
-    Checkbox(v-model="iUnderstand" :disabled="promiseRunning")
-        b I agree to delete my account.
+    .inner
+        .page-title Delete Account
 
-    br
-    br
-    br
+        br
 
-    .bottom
-        div(v-if="promiseRunning" style="width:100%; text-align:center")
-            .loader(style="--loader-color:blue; --loader-size:12px")
-        template(v-else)
-            button.inline.gray(type="button" @click="router.push('/account-setting')") Cancel
-            | &nbsp;&nbsp;
-            button.inline.red(:class="{disabled: !iUnderstand}" @click='processDelete') Delete
+        .page-desc Once you delete your account you will not be able to recover it.
 
-br
-br
-br
+        .page-desc
+            | All your data will be deleted permanently.
+            br
+            | All your subscription plans will be cancelled.
+            br
+            | All your services will be removed.
+            br
+            | All your subdomains will be removed immediately.
+            br
+            | The remaining subscription cannot be refunded.
+
+        .page-desc Are you sure you want to delete your account?
+
+        Checkbox(v-model="iUnderstand" :disabled="promiseRunning")
+            b I agree to delete my account.
+
+        br
+        br
+        br
+
+        .flex-wrap.end
+            button.inline.gray.btn-cancel(type="submit" :class="{disabled: !iUnderstand, red: iUnderstand}" @click='processDelete' style="width: 148px") 
+                template(v-if="promiseRunning")
+                    .loader(style="--loader-color:white; --loader-size:12px")
+                template(v-else) Delete Account
+
 </template>
 
 <script setup lang="ts">
@@ -87,16 +82,35 @@ let processDelete = async () => {
 
 <style scoped lang="less">
 #confirmation {
-    max-width: 640px;
-    padding: 0 20px;
+    max-width: 1500px;
     margin: 0 auto;
+    padding: 60px 40px;
 
-    .bottom {
-        min-height: 44px;
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: flex-end;
+    .inner {
+        margin: 0 auto;
+        text-align: center;
+    }
+}
+
+@media (max-width: 800px) {
+    #confirmation {
+        padding: 60px 20px;
+    }
+
+    .btn-prev {
+        padding: 0.875rem 0;
+
+        &:hover {
+            &::after {
+                opacity: 0;
+            }
+        }
+    }
+}
+
+@media (max-width: 430px) {
+    #confirmation {
+        padding: 30px 20px;
     }
 }
 </style>
