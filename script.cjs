d4cb145626787b0457d4746fc6682a282f8feeed
gyupp <kkb75281@gmail.com>
2025-08-08 18:49:59 +0900

    fix: update build time (dev에도 추가)

--- script.cjs
+++ script.cjs
@@ -1,35 +1,38 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Read file synchronously
-let data = fs.readFileSync('.env.production', 'utf8');
+let data = fs.readFileSync(".env.production", "utf8");
+let data2 = fs.readFileSync(".env.development", "utf8");
 let date = new Date();
 let newDate = date.toLocaleString();
 
 data = data.replace(/(VITE_DATE\s*=\s*")[^"]*(")/, `$1${newDate}$2`);
+data2 = data2.replace(/(VITE_DATE\s*=\s*")[^"]*(")/, `$1${newDate}$2`);
 
-fs.writeFileSync('.env.production', data);
+fs.writeFileSync(".env.production", data);
+fs.writeFileSync(".env.development", data2);
 
-const https = require('https');
+// const https = require('https');
 
-const url = 'https://broadwayinc.com/pp/skapi.html';
+// const url = 'https://broadwayinc.com/pp/skapi.html';
 
-https.get(url, (response) => {
-    let html = '';
+// https.get(url, (response) => {
+//     let html = '';
 
-    // A chunk of data has been received
-    response.on('data', (chunk) => {
-        html += chunk;
-    });
+//     // A chunk of data has been received
+//     response.on('data', (chunk) => {
+//         html += chunk;
+//     });
 
-    // The whole response has been received
-    response.on('end', () => {
-        fs.writeFile('public/pp.html', html, (err) => {
-            if (err) {
-                console.error('파일을 쓰는 중 오류가 발생했습니다:', err);
-                return;
-            }
-        });
-    });
-}).on('error', (error) => {
-    throw 'Error making HTTP request:';
-});
+//     // The whole response has been received
+//     response.on('end', () => {
+//         fs.writeFile('public/pp.html', html, (err) => {
+//             if (err) {
+//                 console.error('파일을 쓰는 중 오류가 발생했습니다:', err);
+//                 return;
+//             }
+//         });
+//     });
+// }).on('error', (error) => {
+//     throw 'Error making HTTP request:';
+// });
